4e8e66249fed5e4105901161be1562f702d07d33
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-03-04 15:10:57 +0700

    sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -8,7 +8,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('dashboard/guest*')? 'active' : '' }}" href="/dashboard/reserve">
+                <a class="nav-link {{ Request::is('dashboard/guest*')? 'active' : '' }}" href="/dashboard/guest">
                     <span data-feather="book-open"></span>
                     Guest List
                 </a>
