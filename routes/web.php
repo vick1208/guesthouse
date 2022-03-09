88911912a7402c2a9f418bc97712b50e8f494ed8
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-03-09 20:13:23 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\GuestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\RoomController;
 use GuzzleHttp\Middleware;
 
