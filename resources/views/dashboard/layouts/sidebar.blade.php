781f267bae3c17012fe3ac7b88fcc7a9006c97ff
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-03-10 14:47:12 +0700

    sidebar update

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -11,7 +11,7 @@
 
             <li class="nav-item">
                 <a class="nav-link {{ Request::is('dashboard/user*')? 'active' : '' }}" href="/dashboard/user">
-                    <span data-feather="book"></span>
+                    <span data-feather="user"></span>
                     User List
                 </a>
             </li>
