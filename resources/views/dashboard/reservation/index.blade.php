9bbed632698e02f1f19e78f6ac0e2cafbd2c8b7c
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-03-15 12:55:05 +0700

    reservation update

--- resources/views/dashboard/reservation/index.blade.php
+++ resources/views/dashboard/reservation/index.blade.php
@@ -51,15 +51,20 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">New Guest?</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <div class="d-flex justify-content-center">
+                        <a class="btn btn-sm btn-success m-1"
+                            href="/dashboard/guest/create">Yes, create
+                            new guest!</a>
+                        {{-- <a class="btn btn-sm btn-success m-1"
+                            href="/dashboard/register/create">Yes, guest has been recorded!</a> --}}
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
@@ -77,7 +82,6 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
