c5f4cf0ec42001d4211c2edc4b864befa15af3ac
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-06-14 19:39:59 +0700

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 
 @section('container')
-    <h1 class="text-center">Guest House D'Strawberry</h1>
+
+
+    <img src="{{ asset('img/Exterior guest house.png') }}" alt="halaman depan guest house" width="600px"  class="rounded mx-auto d-block img-fluid">
+
+    <h1 class="text-center fw-light">Guest House D'Strawberry</h1>
+
+    <p class="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat a ullam illum, et voluptatibus nostrum tempora ratione praesentium, adipisci officia aspernatur fugit maxime dolores accusantium nihil quidem blanditiis obcaecati ducimus sequi corporis quod vero qui ipsa. Qui, atque. Aperiam ratione ex ab ipsa, voluptates necessitatibus rem harum dolor accusantium cum recusandae iure, labore impedit unde eius minima fuga, cumque earum! Minus consectetur accusantium deserunt repellendus doloribus explicabo nisi! Alias voluptatem reiciendis nam vel veniam perspiciatis est nulla aperiam, eos similique illum, deserunt, placeat laborum animi deleniti reprehenderit? Eos, rerum nesciunt architecto laboriosam voluptate blanditiis cupiditate neque esse doloribus nulla magnam.
+    </p>
+
 @endsection
 
 
