ae4a687289d5e3b134800aade58c82e4178bc92e
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-06-14 20:21:54 +0700

    navbar update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #db6930;">
     <div class="container">
         <a class="navbar-brand" href="/">Guest House </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
